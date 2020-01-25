17e15616ba2bcb7339f45caf66f7d8c44b82224e
seanheinrichs <seanheinrichs@gmail.com>
2020-01-25 18:09:29 -0500

    allow user to toggle between roasted and toasted modes

--- frontend/src/pages/homepage/homepage.component.jsx
+++ frontend/src/pages/homepage/homepage.component.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
 import Camera from 'react-html5-camera-photo';
+import Typography from '@material-ui/core/Typography';
 import NavBar from '../../components/navbar/navbar.component';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 import 'react-html5-camera-photo/build/css/index.css';
+import 'typeface-patrick-hand';
 
 const HomePage = () => {
 
     const _handleTakePhoto = (dataUri) => {
-        fetch("http://localhost:3001/insults/list")
+        console.log(dataUri);
+        fetch('http://localhost:3001/insults/list')
         .then(response => response.json())
         .then( responseJson => {
-            console.log(responseJson);
             alert(responseJson.data[0].insult);
         },
     )}
 
     return (
         <div className='homepage'>
+            <Typography variant="h3">Humble Me</Typography>
             <NavBar />
             <Camera 
                 onTakePhoto= { (dataUri) => { _handleTakePhoto(dataUri); } }
