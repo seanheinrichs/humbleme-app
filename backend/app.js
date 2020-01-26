039f3ce3ec48e67007bd1fe83c214d551e5a117d
yaroslaviy <yaselokeev@gmail.com>
2020-01-25 22:41:03 -0500

    connected front and backend

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,19 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(express.json());
 app.post("/photo", (req, res) => {
+  const b64string = req.body.photo;
+  const buf = Buffer.from(b64string, 'base64');
+  let params = {
+    CollectionId: "humbleme", 
+    FaceMatchThreshold: 10, 
+    Image: {
+        Bytes: buf
+    }, 
+    MaxFaces: 5
+   };
+  apiFunctionWrapper(params, (err, data) => {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data.FaceMatches[0].Face.FaceId, data.FaceMatches[0].Face);  }); 
   try {
     res.status(200).send({
       message: "Roast Goes Here!"
--- backend/dummyDatabase.js
+++
@@ -1,8 +0,0 @@
-let insults = [
-    { _id: 1, name: "Sean", insult: "You're ugly" },
-    { _id: 2, name: "Mike", insult: "You're gay" },
-    { _id: 3, name: "Zach", insult: "You're mean" },
-    { _id: 4, name: "Yaroslav", insult: "You're ridiculous" }
-];
-
-module.exports = insults;
